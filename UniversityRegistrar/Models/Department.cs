41e1fbd735efc6cc1dfe4007d972e1d78aa86c39
zbl14 <ifthereisoneday@gmail.com>
2022-08-02 22:09:32 -0700

    update Department.cs

--- UniversityRegistrar/Models/Department.cs
+++ UniversityRegistrar/Models/Department.cs
@@ -4,12 +4,13 @@ namespace UniversityRegistrar.Models
 {
   public class Department
   {
-    public Department ()
+    public Department()
     {
       this.Students = new HashSet<Student>();
       this.Courses = new HashSet<Course>();
     }
-    public int DepartmentId { get; set; }
+    
+    public int DepartmentId { get; set; } 
     public string DepartmentName { get; set; }
     public virtual ICollection<Student> Students { get; set; }
     public virtual ICollection<Course> Courses { get; set; }
